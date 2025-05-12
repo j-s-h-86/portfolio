824ff58d11cbb7ab799a67a416e3f238bbae5f4a
johan.hammarstedt@j-s-h.com <johan.hammarstedt@j-s-h.com>
2025-05-12 10:04:21 +0200

    header and footer

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<footer>
+	<p>
+		&copy; {new Date().getFullYear()}
+	</p>
+	<p>Johan Hammarstedt</p>
+	<p>Fullständiga rättigheter.</p>
+</footer>
+
+<style>
+	footer {
+		height: 40px;
+		width: 100%;
+		background-color: black;
+		border-top: 1px solid white;
+		color: white;
+		margin-bottom: 0;
+		padding: 10px;
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+	}
+</style>
---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<header>
+	<h3>Johan Hammarstedt</h3>
+</header>
+
+<style>
+	header {
+		width: 100%;
+		height: 75px;
+		background-color: black;
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+		padding: 10px;
+	}
+
+	h3 {
+		color: white;
+	}
+</style>
